ee8b3a33dab00a7b28cdd66492319429fd99815d
Mary Skywalker <snowdennaruto90@gmail.com>
2022-12-28 21:23:27 -0400

    simulating a database

--- src/schema/schema.ts
+++
@@ -1,7 +0,0 @@
-import path from "path";
-import { buildSchema } from "type-graphql";
-
-export const schema = await buildSchema({
-  resolvers: [],
-  emitSchemaFile: path.resolve(__dirname, "schema.gql"),
-});
