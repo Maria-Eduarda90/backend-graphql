6d1548cd170110a49d97371219ed808b5f98d3f9
Mary Skywalker <snowdennaruto90@gmail.com>
2022-12-28 21:36:02 -0400

    finished

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,5 +1,6 @@
-import { Query, Resolver } from 'type-graphql';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
 import { User } from '../models/User';
+import crypto from 'crypto';
 
 @Resolver()
 export class UserResolver {
@@ -8,5 +9,23 @@ export class UserResolver {
     @Query(() => [User])
     async users(){
         return this.data;
+    }
+
+    @Mutation(() => User)
+    async createUser(
+        @Arg('name') name: string,
+        @Arg('email') email: string,
+        @Arg('password') password: string
+    ){
+        const user = {
+            id: crypto.randomUUID(),
+            name,
+            email,
+            password
+        }
+
+        this.data.push(user);
+
+        return user;
     }
 }
